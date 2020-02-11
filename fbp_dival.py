22ab13caf78e42a56d1da4a54da52a5e2b211118
Pedro Soares <pscoutosoares@gmail.com>
2020-02-11 05:27:16 -0300

    Update fbp_dival.py

--- fbp_dival.py
+++ fbp_dival.py
@@ -6,65 +6,89 @@ from dival.datasets.standard import get_standard_dataset
 from dival.util.constants import MU_MAX
 from pathlib import Path
 
+      
 # %% Apenas modificar essas variáveis 
 #step (train, test or validation)
 step = 'test'
-initial_slice = 10 
-final_slice = 20
+initial_slice = 0 
+final_slice = 0
+samples = 1000
 
 
 dataset = get_standard_dataset('lodopab', observation_model='pre-log')
+
+if(step == 'train'):
+    step_len = dataset.train_len
+elif(step == 'validation'):
+    step_len = dataset.validation_len
+else:
+    step_len = dataset.test_len
+
 ray_trafo = dataset.get_ray_trafo(impl='astra_cpu')
 reco_space = ray_trafo.domain
 
-"""
-get_data_pairs:
-    @args:
-        'tipo da base(test,train ou validation)'
-        'slice inicial',
-        'slice final',
-    Os slices definem o intervalo que deverá ser utilizado da base de dados
-    Caso não defina um intervalo, toda a base será utilizada
-"""
-test_data = dataset.get_data_pairs(step, initial_slice, final_slice)
+index_j = step_len//samples
+for index_i in range(0, index_j):
 
-# %% task table and reconstructors
-eval_tt = TaskTable()
+    initial_slice = final_slice
+    if ((index_i) == index_j):
+        final_slice += (step_len % samples)
+    else: 
+        final_slice += samples
 
-"""
-A configuração filter_type com Ram-Lak e frequency_scaling 1.0 foram as utilizadas no artigo
-"""
-fbp_reconstructor = FBPReconstructor(
-    ray_trafo, hyper_params={
-        'filter_type': 'Ram-Lak',
-        'frequency_scaling': 1.0},
-    pre_processor=(-1/MU_MAX) * log_op(ray_trafo.range))
+    """
+    !!!!!!!!!!!!!!!!!!!!!!!!Importante!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    get_data_pairs:
+        @args:
+            'tipo da base(test,train ou validation)'
+            'slice inicial',
+            'slice final',
+        Os slices definem o intervalo que deverá ser utilizado da base de dados
+        Caso não defina um intervalo, toda a base será utilizada
+    """
+    test_data = dataset.get_data_pairs(step, initial_slice, final_slice )
 
-reconstructors = [fbp_reconstructor]
+    # %% task table and reconstructors
+    eval_tt = TaskTable()
 
-eval_tt.append_all_combinations(reconstructors=reconstructors,
-                                test_data=[test_data])
+    """
+    A configuração filter_type com Ram-Lak e frequency_scaling 1.0 foram as utilizadas no artigo
+    """
+    fbp_reconstructor = FBPReconstructor(
+        ray_trafo, hyper_params={
+            'filter_type': 'Ram-Lak',
+            'frequency_scaling': 1.0},
+        pre_processor=(-1/MU_MAX) * log_op(ray_trafo.range))
 
-# %% run task table
-results = eval_tt.run()
-results.apply_measures([PSNR, SSIM])
-print(results)
+    reconstructors = [fbp_reconstructor]
 
-# %% plot reconstructions
+    eval_tt.append_all_combinations(reconstructors=reconstructors,
+                                    test_data=[test_data])
+
+    # %% run task table
+    results = eval_tt.run()
+    results.apply_measures([PSNR, SSIM])
+    print(results)
+
+    # %% plot reconstructions
 
 
-Path(step).mkdir(parents=True, exist_ok=True)
-Path(step + '_gt').mkdir(parents=True, exist_ok=True)
+    Path(step).mkdir(parents=True, exist_ok=True)
+    Path(step + '_gt').mkdir(parents=True, exist_ok=True)
 
 
-"""Plot all reconstructions.
+    """Plot all reconstructions.
 
-        Parameters
-        ----------
-        step :          (train, test or validation)
-        test_ind :      range(x); quantidade de plots que serão salvos
-                        se test_ind for igual a -1, serão plotados todos os elementos
-        initial_slice:  parte inicial das amostras  
-"""
-fig = results.plot_all_reconstructions(step, initial_slice, test_ind=-1,
-                                       fig_size=(9, 4), vrange='individual')
+            Parameters
+            ----------
+            step :          (train, test or validation)
+            test_ind :      range(x); quantidade de plots que serão salvos
+                            se test_ind for igual a -1, serão plotados todos os elementos
+            initial_slice:  parte inicial das amostras  
+    """
+    fig = results.plot_all_reconstructions(step, initial_slice, test_ind=-1,
+                                        fig_size=(9, 4), vrange='individual')
+
+
+
+# %%
